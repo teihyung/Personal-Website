b0dbf068d68aada95ff1c9af8cc5a8a07f114f61
teihyung <teihyung@gmail.com>
2025-11-16 17:39:20 +0900

    force pushing directory issue directly in main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Router from "./Router";
+import Router from "./router";
 
 function App() {
   return <Router />;
